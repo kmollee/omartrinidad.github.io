62787e01c593e60956ce58bb582bc494af476b6f
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2013-07-27 14:09:49 -0500

    Changes

---
+++ pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'omartrinidad'
+SITENAME = u'Omar swap file'
+SITEURL = 'omartrinidad.github.io'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Added after pelican-quickstart
+
+THEME = '/home/omartrinidad/projects/darktheme'
